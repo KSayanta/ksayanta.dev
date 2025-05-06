b7eecfe3f29e6c23152ac865e5f62da07f9cddbe
KSayanta <sayantakarmakar1999@gmail.com>
2025-05-06 17:49:54 +0530

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
+
 import { getFromGenAI } from "./ai.js";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/api", async (req, res) => {
