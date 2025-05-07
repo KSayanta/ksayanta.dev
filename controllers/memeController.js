eea6255bab73c464bcaad8fe346dcf6987066a99
KSayanta <sayantakarmakar1999@gmail.com>
2025-05-07 21:26:22 +0530

    feat: get random meme from top 100 memes

--- controllers/memeController.js
+++ controllers/memeController.js
@@ -1,6 +1,13 @@
 const url = "https://api.imgflip.com/get_memes";
 
-export async function memeCtrl(_, res) {
+export async function getMeme(_, res) {
   const data = await fetch(url).then((res) => res.json());
   res.json(data);
+}
+
+export async function randMeme(_, res) {
+  const data = await fetch(url).then((res) => res.json());
+  const memeArr = data.data.memes;
+  const meme = memeArr[Math.floor(memeArr.length * Math.random())];
+  res.json(meme);
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { recipeCtrl } from "../controllers/recipeController.js";
-import { memeCtrl } from "../controllers/memeController.js";
+import { getMeme, randMeme } from "../controllers/memeController.js";
 
 export const apiRouter = express.Router();
 
-apiRouter.get("/meme", memeCtrl);
+apiRouter.get("/meme", getMeme);
+apiRouter.get("/rand_meme", randMeme);
 
 apiRouter.post("/recipe", recipeCtrl);
