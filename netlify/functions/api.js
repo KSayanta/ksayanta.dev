ff1613a489b40612938f431bbe7e32837b2911c7
KSayanta <sayantakarmakar1999@gmail.com>
2025-05-14 16:30:29 +0530

    add netlify support

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,23 +1,18 @@
 import express from "express";
-import "dotenv/config";
 import cors from "cors";
+import serverless from "serverless-http";
 import { apiRouter } from "./routes/apiRoutes.js";
 
-const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json());
-app.use("/api", apiRouter);
+app.use("/api/", apiRouter);
 
 app.use((req, res) => {
   console.log(`Incoming Connection from: ${req.ip}`);
   res.status(404).send("Page does not exist");
 });
 
-app.listen(PORT, (err) => {
-  if (err) console.error(err.message);
-
-  console.log(`server connected at ${PORT}`);
-});
+export const handler = serverless(app);
--- netlify/functions/routes/apiRoutes.js
+++ netlify/functions/routes/apiRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { hello } from "../controllers/helloWorld.js";
 import { recipeCtrl } from "../controllers/recipeController.js";
 import { getMeme, randMeme } from "../controllers/memeController.js";
 
 export const apiRouter = express.Router();
 
+apiRouter.get("/", hello);
 apiRouter.get("/meme", getMeme);
 apiRouter.get("/rand_meme", randMeme);
 
