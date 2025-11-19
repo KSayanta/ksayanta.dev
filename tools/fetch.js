6c1c4f3fcdca49b184976d89750499d65519b48e
KSayanta <181443436+KSayanta@users.noreply.github.com>
2025-11-19 19:59:54 +0530

    update fetch script

--- tools/fetch.js
+++ tools/fetch.js
@@ -1,9 +1,43 @@
 import { writeFileSync } from "fs";
+import retry from "retry";
 
 const URL = "https://osu.ppy.sh/api/v2/seasonal-backgrounds";
 const OUT_FILE = "current_seasonal.json";
 
-const res = await fetch(URL);
-const data = await res.text();
+function fetchWithRetry(url) {
+  const operation = retry.operation({
+    retries: process.env.MAX_RETRIES || 5,
+    minTimeout: 500,
+    maxTimeout: 10000,
+    randomize: true,
+  });
 
-writeFileSync(OUT_FILE, data);
+  return new Promise((resolve, reject) => {
+    operation.attempt(async (currentAttempt) => {
+      try {
+        const res = await fetch(url);
+        const data = await res.text();
+        resolve(data);
+      } catch (err) {
+        if (operation.retry(err)) {
+          console.warn(
+            `Failed to connect on attempt ${currentAttempt}, retrying...`,
+          );
+          return;
+        } else {
+          reject(operation.mainError());
+        }
+      }
+    });
+  });
+}
+
+(async () => {
+  try {
+    const data = await fetchWithRetry(URL);
+    writeFileSync(OUT_FILE, data);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
